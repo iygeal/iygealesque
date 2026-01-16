5f6d4ff55fcc115ff3c3136260c5e321526f658f
Innocent Anoziee <iygeal@gmail.com>
2026-01-16 18:08:11 +0100

    UPDATE: Changed intro message for projects page

--- portfolio/src/app/projects/page.tsx
+++ portfolio/src/app/projects/page.tsx
@@ -8,8 +8,7 @@ export default function ProjectsPage() {
         <h1 className="text-3xl md:text-4xl font-bold">Selected Projects</h1>
 
         <p className="text-slate-300">
-          Real-world backend and fullstack applications designed, deployed, and
-          maintained in production environments.
+          Production-ready backend and fullstack systems designed, deployed, and maintained in live production environments.
         </p>
       </div>
 
